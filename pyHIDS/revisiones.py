1da71c041c27269ebeb2e5e2176827bacccfbaa3
Rubense110 <84941508+Rubense110@users.noreply.github.com>
2022-10-02 20:16:20 +0200

    pdf mejorado

--- pyHIDS/revisiones.py
+++ pyHIDS/revisiones.py
@@ -4,13 +4,13 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from lib2to3.pgen2.token import COLON
 import smtplib
 import conf
 from fpdf import FPDF
 import os
 import matplotlib.pyplot as plt
 import sys
-
 
 PATH = os.path.abspath(".")
 
@@ -51,17 +51,21 @@ def craftea_grafico(accesos,errores,warnings,fecha):
     legend = ["Accesos sin incidencia"]
     etiquetas=[str(accesos_normales)]
     explode=[0]
-
-    if(errores!=0):valores.append(errores);legend.append("Errores de autenticacion");explode.append(0.2);etiquetas.append(str(errores))
-    if(warnings!=0):valores.append(warnings);legend.append("Warnings");explode.append(0.2);etiquetas.append(str(warnings))
+    colors=["lightskyblue"]
+    if(errores!=0):valores.append(errores);legend.append("Errores");explode.append(0.2);etiquetas.append(str(errores));colors.append("lightcoral")
+    if(warnings!=0):valores.append(warnings);legend.append("Warnings");explode.append(0.2);etiquetas.append(str(warnings));colors.append("gold")
 
     etiquetas = tuple(etiquetas)
-    f1={"family": "arial","color": "black", "size": 20, "fontweight": "roman"}
-    
+    f1={"family": "Times New Roman","color": "black", "size": 20, "fontweight": "roman"}
+    porcent= list()
+    for i in valores:
+        porcent.append(100.*float(i)/sum(valores))
+
+    labels = ['{0} - {1:1.2f} %'.format(i,j) for i,j in zip(legend, porcent )]
     plt.figure(figsize=(7,3))
-    plt.pie(valores,startangle=90,shadow=True, explode=explode, autopct="%2.2f%%")
+    plt.pie(valores,startangle=90,shadow=True,explode=explode, colors=colors)
     plt.title("Accesos al Sistema de Archivos\n",fontdict=f1)
-    plt.legend(legend,loc="upper left",fontsize="x-small")
+    plt.legend(labels,loc="upper left",fontsize="x-small")
     plt.axis("equal")
     plt.savefig(os.path.join(PATH,"revisiones","graficos","grafico"+fecha.replace("/","-")+".png"),bbox_inches='tight',dpi=300)
 
@@ -70,15 +74,16 @@ def craftea_pdf(fecha,accesos,errores,warnings):
 
     pdf = FPDF()
     pdf.add_page()
-    
-    pdf.set_font('Arial', 'B', 40)
+    pdf.set_font('Times', 'B', 40)
+    pdf.cell(200, 10, txt = "",ln = 1, align = 'C')
     pdf.cell(200, 10, txt = "HIDS INSEGUS",ln = 1, align = 'C')
-    pdf.set_font('Arial', 'B', 16)
+    pdf.set_font('Times', 'B', 16)
     pdf.cell(200, 10, txt = "Reporte "+fecha+"\n\n" ,ln = 1, align = 'C')
-    pdf.image(os.path.join(PATH,"revisiones","graficos","grafico"+fecha.replace("/","-")+".png"),20,40, h=100,w=180)
-    pdf.cell(200, 120, txt = "",ln = 1, align = 'L')
+    pdf.image(os.path.join(PATH,"revisiones","graficos","grafico"+fecha.replace("/","-")+".png"),20,50, h=100,w=180)
+    pdf.cell(200, 100, txt = "",ln = 1, align = 'L')
     pdf.set_left_margin(32)
-    pdf.set_font("Arial",style="")
+    pdf.set_font("Times",style="")
+    pdf.cell(200, 20, txt = "",ln = 1, align = 'C')
     pdf.cell(200, 10, txt = "· Nº de errores detectados: "+errores,ln = 1, align = 'L')
     pdf.cell(200, 10, txt = "· Nº de warnings detectados: "+warnings,ln = 1, align = 'L')
     pdf.cell(200, 10, txt = "· Nº total de accesos: "+accesos,ln = 1, align = 'L')
